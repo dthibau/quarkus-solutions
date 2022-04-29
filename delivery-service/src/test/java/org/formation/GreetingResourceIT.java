73c817ce427dbdb7da8927a52aadbd1b57e1b8d0
David THIBAU <david.thibau@gmail.com>
2022-04-29 14:01:42 +0200

    Atelier1

---
+++ delivery-service/src/test/java/org/formation/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.formation;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
