a2dd67a845aae1137ab63b15d0a02f7d9b180148
David THIBAU <david.thibau@gmail.com>
2024-01-08 10:51:56 +0100

    Profils

--- delivery-service/src/test/java/org/formation/LivraisonResourceTest.java
+++ delivery-service/src/test/java/org/formation/LivraisonResourceTest.java
@@ -4,17 +4,16 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-class GreetingResourceTest {
+class LivraisonResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/livraisons")
           .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .statusCode(200);
+            // .body(is("Hello from RESTEasy Reactive"));
     }
 
 }
