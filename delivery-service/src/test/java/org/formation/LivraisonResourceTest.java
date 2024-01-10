6a386c640de165fa31a2efeda86b3eb64fd1616f
David THIBAU <david.thibau@gmail.com>
2024-01-10 16:16:58 +0100

    Tests

--- delivery-service/src/test/java/org/formation/LivraisonResourceTest.java
+++ delivery-service/src/test/java/org/formation/LivraisonResourceTest.java
@@ -1,19 +1,23 @@
 package org.formation;
 
+import io.quarkus.logging.Log;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @QuarkusTest
 class LivraisonResourceTest {
     @Test
-    void testHelloEndpoint() {
+    void wheGetLivraisonsCheckStatusAndSizeofArray() {
         given()
           .when().get("/livraisons")
           .then()
-             .statusCode(200);
-            // .body(is("Hello from RESTEasy Reactive"));
+             .statusCode(200)
+             .body("$", hasSize(4))
+             .body("[0].id", equalTo(1));
+
     }
 
 }
