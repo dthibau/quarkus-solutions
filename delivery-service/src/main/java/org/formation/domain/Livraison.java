2eed7ff868554633a7bf06ddea8a495f871a9810
David THIBAU <david.thibau@gmail.com>
2024-01-10 17:17:53 +0100

    Hibernate avec H2

--- delivery-service/src/main/java/org/formation/domain/Livraison.java
+++ delivery-service/src/main/java/org/formation/domain/Livraison.java
@@ -2,23 +2,63 @@ package org.formation.domain;
 
 import java.time.Instant;
 
+
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Livraison {
 
-	private long id;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
 	
-	private String noCommande;
+	public String noCommande;
 	
-	private Livreur livreur;
+	@OneToOne
+	public Livreur livreur;
 	
-	private Status status;
+	@NotNull
+	public Status status;
 	
+	public Instant creationDate;
 
-	private Instant creationDate;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Livraison other = (Livraison) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 	
 	
 
--- delivery-service/src/main/java/org/formation/domain/Livreur.java
+++ delivery-service/src/main/java/org/formation/domain/Livreur.java
@@ -2,17 +2,33 @@ package org.formation.domain;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Livreur {
 
-	private long id;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
 	
-	private String nom;
+	public String nom;
 	
-	private String telephone;
+	public String telephone;
 
-	private List<Review> reviews;
+	@OneToMany(cascade = CascadeType.ALL)
+	public List<Review> reviews;
+	
 }
