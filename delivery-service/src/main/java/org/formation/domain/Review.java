4305263a2e160f86c552c4654762c5e9e6497866
David THIBAU <david.thibau@gmail.com>
2024-01-10 17:17:53 +0100

    Hibernate avec H2

--- delivery-service/src/main/java/org/formation/domain/Review.java
+++ delivery-service/src/main/java/org/formation/domain/Review.java
@@ -1,13 +1,23 @@
 package org.formation.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Review {
 
-	private long id;
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public long id;
 	
-	private int note;
+	public int note;
 	
-	private String commentaire;
+	public String commentaire;
 }
