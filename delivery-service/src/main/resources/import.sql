4470492024bc8cee3f4bd6ca6813e38b26152870
David THIBAU <david.thibau@gmail.com>
2024-01-11 12:02:17 +0100

    Panache EntityPattern

--- delivery-service/src/main/resources/import.sql
+++ delivery-service/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-insert into livreur (id,nom,telephone) values (1,'Speedy Gonzales','06-49-89-69-19');
+insert into livreur (id,nom,telephone,status) values (1,'Speedy Gonzales','06-49-89-69-19','ACTIF');
 
