9ea5ae8ac7b60c5d9f0e4b1fc8c8abf04c23ed03
David THIBAU <david.thibau@gmail.com>
2024-01-12 14:10:10 +0100

    @JsonView

--- delivery-service/src/main/resources/import.sql
+++ delivery-service/src/main/resources/import.sql
@@ -1,4 +1,6 @@
 insert into livreur (id, nom,telephone,status) values (1, 'Speedy Gonzales','06-49-89-69-19','ACTIF');
+insert into review(id, note, commentaire) values (1, 5, 'Super livreur');
+insert into Livreur_Review (livreur_id, reviews_id) values (1, 1);
 
 insert into livraison ( noCommande, creationDate, status, livreur_id) values ( 1, '2024-01-01 12:00:00', 'LIVREUR_AFFECTE', 1);
 insert into livraison ( noCommande, creationDate, status, livreur_id) values ( 2, '2024-01-01 12:00:00', 'CREE', null);
