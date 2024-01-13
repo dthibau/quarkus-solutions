24e686f63e938b48be4903483c92bd1292dcec26
David THIBAU <david.thibau@gmail.com>
2024-01-13 12:52:27 +0100

    Publishing Message

--- order-service/src/main/java/org/formation/service/OrderService.java
+++ order-service/src/main/java/org/formation/service/OrderService.java
@@ -9,6 +9,8 @@ import org.formation.domain.Order;
 import org.formation.domain.OrderItem;
 import org.formation.domain.OrderRepository;
 import org.formation.domain.PaymentInformation;
+import org.formation.domain.event.OrderEvent;
+import org.formation.domain.event.OrderEventType;
 
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.quarkus.logging.Log;
@@ -18,25 +20,41 @@ import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class OrderService {
-    
+
     @RestClient
+    @Inject
     NotificationService notificationService;
+
+    @Inject
+    PublishService publishService;
 
     @Inject
     OrderRepository orderRepository;
 
     @WithTransaction
-    public Uni<Order> createOrder(List<OrderItem> lineItems, Address deliveryAddress, PaymentInformation paymentInformation) {
+    public Uni<Order> createOrder(List<OrderItem> lineItems, Address deliveryAddress,
+            PaymentInformation paymentInformation) {
         Order order = new Order();
-		DeliveryInformation df = new DeliveryInformation();
-		df.setAddress(deliveryAddress);  
-		order.setDeliveryInformation(df);
-		order.setOrderItems(lineItems);
-		order.setPaymentInformation(paymentInformation);
+        DeliveryInformation df = new DeliveryInformation();
+        df.setAddress(deliveryAddress);
+        order.setDeliveryInformation(df);
+        order.setOrderItems(lineItems);
+        order.setPaymentInformation(paymentInformation);
 
-        NotificationDto dto = NotificationDto.builder().to("david.thibau@gmail.com").subject("Order created").text(order.toString()).build();
-        notificationService.sendMail(dto).subscribe().with(n -> Log.info("Notification sent " + n), 
-                                                           f -> Log.error("Error sending notification " + f));
-        return orderRepository.persist(order);
+        return orderRepository.persistAndFlush(order)
+                .onItem().invoke(pesistedOrder -> {
+                    Log.info("Order created " + pesistedOrder.getId() + " about to notify");
+                    NotificationDto dto = NotificationDto.builder().to("david.thibau@gmail.com")
+                            .subject("Order created").text(order.toString()).build();
+                    notificationService.sendMail(dto).subscribe().with(
+                            n -> Log.info("Notification sent " + n),
+                            f -> Log.error("Error sending notification " + f));
+                })
+                .onItem().invoke(pesistedOrder -> {
+                    Log.info("Order created " + pesistedOrder.getId() + " about to publish");
+                    publishService
+                            .publish(OrderEvent.builder().type(OrderEventType.CREATED).order(pesistedOrder).build());
+                })
+                ;
     }
 }
