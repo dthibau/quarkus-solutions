c50a666e84c8020761ce0e2708e4ecbd6731bb55
David THIBAU <david.thibau@gmail.com>
2024-01-15 10:43:36 +0100

    Propagation du jeton

--- gateway/src/main/java/org/formation/service/OrderService.java
+++ gateway/src/main/java/org/formation/service/OrderService.java
@@ -2,22 +2,26 @@ package org.formation.service;
 
 import java.util.List;
 
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.formation.domain.Order;
 import org.formation.domain.OrderRequest;
 
+import io.quarkus.oidc.token.propagation.reactive.AccessTokenRequestReactiveFilter;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
+
 @Path("/orders")
 @RegisterRestClient(configKey = "orders-api")
+@RegisterProvider(AccessTokenRequestReactiveFilter.class)
 public interface OrderService {
     
     @GET
     public Uni<List<Order>> findAll();
-    
+
     @POST
     public Uni<Order> createOrder(OrderRequest orderRequest);
 }
