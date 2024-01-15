4e5ae041007cd0f95ba6fd6060dad60db4c8b243
David THIBAU <david.thibau@gmail.com>
2024-01-15 11:41:08 +0100

    order-service utilise son propre jeton vers notification-service

--- notification-service/src/main/java/org/formation/NotificationResource.java
+++ notification-service/src/main/java/org/formation/NotificationResource.java
@@ -4,11 +4,13 @@ import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
 import io.quarkus.mailer.reactive.ReactiveMailer;
 import io.smallrye.mutiny.Uni;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 @Path("/notifications")
+@RolesAllowed("default-roles-quarkus")
 public class NotificationResource {
 
 	@Inject Mailer mailer;     
--- order-service/src/main/java/org/formation/service/NotificationService.java
+++ order-service/src/main/java/org/formation/service/NotificationService.java
@@ -1,14 +1,17 @@
 package org.formation.service;
 
 
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
+import io.quarkus.oidc.client.reactive.filter.OidcClientRequestReactiveFilter;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 
 @Path("/notifications")
 @RegisterRestClient(configKey = "notification-api")
+@RegisterProvider(OidcClientRequestReactiveFilter.class)
 public interface NotificationService {
     
     @POST
